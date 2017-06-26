0317387513a8bd9bc7482d5fe254074136c3b352
RyanYoung <ryanayoung81@gmail.com>
2017-06-25 19:53:43 -0700

    added basic canbus project, removed bad project

--- can_bus/can_bus/Debug/main.d
+++ can_bus/can_bus/Debug/main.d
@@ -13,9 +13,12 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././headers/global.h .././headers/defines.h .././headers/spi_ry.h \
- .././headers/usart_ry.h .././headers/mcp2515_ry_def.h \
- .././headers/mcp2515_ry.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
+ .././headers/global.h .././headers/defines.h .././headers/functions.h \
+ .././headers/spi_ry.h .././headers/usart_ry.h \
+ .././headers/mcp2515_ry_def.h .././headers/mcp2515_ry.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -45,9 +48,17 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+
 .././headers/global.h:
 
 .././headers/defines.h:
+
+.././headers/functions.h:
 
 .././headers/spi_ry.h:
 
